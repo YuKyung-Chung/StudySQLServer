bdb86877dc89c89ecd870220d3972b99be12a590
admin <admin@DESKTOP-V8UOJ36>
2021-05-12 12:48:22 +0900

    SQL 시작

---
+++ Sample/210512_연습.sql
@@ -0,0 +1,7 @@
+SELECT memberID, memberName
+	FROM memberTBL
+	WHERE memberID = 'Dang';
+--SELECT productName, cost, company FROM productTBL;
+
+INSERT INT() memberTBL
+VALUES ('Bang','방문식','부산시 진구');
